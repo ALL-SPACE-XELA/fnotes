8c6187f7e572c89e991bfc456bc342c2a169a2eb
6367f766 <78221071+6367f766@users.noreply.github.com>
2024-11-14 22:30:39 +0000

    Open note buffer relative to cursor position
    Allows the note to be opened around (below) the cursor position. This
    commit also includes a refactor of open/close buffer actions
    
    Signed-off-by: 6367f766 <78221071+6367f766@users.noreply.github.com>

--- plugin/fnote/fnote.py
+++ plugin/fnote/fnote.py
@@ -36,6 +36,40 @@ def check_has_fnote(file: str):
         vim.command('exe ":silent :sign place 2 line=3 name=NFILE file=" . expand("%:p")')
 
 
+def close_buffer(window_handle, buffer_handle, nfile):
+    vim.command('set virtualedit=""')
+    vim.command('let g:indentLine_enabled = 1')
+    if window_handle in vim.api.list_wins():
+        # FIXME: we only save a max of 100 lines...
+        lines = vim.api.buf_get_lines(buffer_handle, 0, 100, False)
+        vim.api.win_close(window_handle, True)
+        nfile.dump(lines)
+
+def open_buffer(nfile, col=0, row=1):
+    # on buffer open we set virtual edit to "all"
+    vim.command("set virtualedit=all")
+    vim.command('let g:indentLine_enabled = 0')
+    buffer = vim.api.create_buf(False, True)
+    # row + 3 to allow 3 lines of space from cursor position and buffer window
+    # col = 1 to disable it from swinging left-right
+    vim.api.open_win(buffer, True, {'relative': 'win', 'width': 100, 'height': 40, 'col': 1,
+                                    'row': row, 'style': 'minimal', 'border': 'single', 'fixed': False})
+
+    vim.api.buf_set_option(buffer, 'modifiable', True)
+    vim.api.command('set filetype=markdown')
+
+    #if DEBUG:
+    #    lines = DEBUG_INFO.format(file, result, current_line, is_buffer_open, window_handle).split("\n")
+    #    lines = [x for x in lines if x != ""]
+    #else:
+    lines = []
+
+    lines.extend(nfile.get_lines())
+
+    vim.api.buf_set_lines(buffer, 0, 0, True, lines)
+    # after open we want to go to the top
+    vim.command(":1")
+
 
 
 @is_buffer_open
@@ -54,37 +88,10 @@ def main(is_buffer_open: bool, window_handle: str, buffer_handle: str, file: str
     result = hash(current_line)
 
     if is_buffer_open:
-        # we close
-        # on buffer close we set virtualedit back to default ("")
-        vim.command('set virtualedit=""')
-        vim.command('let g:indentLine_enabled = 1')
-        if window_handle in vim.api.list_wins():
-            lines = vim.api.buf_get_lines(buffer_handle, 0, 100, False)
-            vim.api.win_close(window_handle, True)
-            nfile.dump(lines)
-            
-
+            # we close
+            # on buffer close we set virtualedit back to default ("")
+            close_buffer(window_handle, buffer_handle, nfile)
     else:
-        # on buffer open we set virtual edit to "all"
-        vim.command("set virtualedit=all")
-        vim.command('let g:indentLine_enabled = 0')
-        buffer = vim.api.create_buf(False, True)
-        vim.api.open_win(buffer, True, {'relative': 'win', 'width': 100, 'height': 40, 'col': 0,
-                                        'row': 1, 'anchor': 'SW', 'style': 'minimal', 'border': 'single'})
-
-        vim.api.buf_set_option(buffer, 'modifiable', True)
-        vim.api.command('set filetype=markdown')
-
-        if DEBUG:
-            lines = DEBUG_INFO.format(file, result, current_line, is_buffer_open, window_handle).split("\n")
-            lines = [x for x in lines if x != ""]
-        else:
-            lines = []
-
-        lines.extend(nfile.get_lines())
-
-
-        vim.api.buf_set_lines(buffer, 0, 0, True, lines)
-        # after open we want to go to the top
-        vim.command(":1")
-
+        # but then we reopen at:
+        row, col = vim.current.window.cursor
+        open_buffer(nfile, col, row)
